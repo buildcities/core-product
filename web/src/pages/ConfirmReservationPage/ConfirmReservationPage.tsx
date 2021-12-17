b4e2821ba7c0bcfab83d56655daa7e035f91b4e9
Tim Jones <t27.jones@connect.qut.edu.au>
2021-12-17 15:53:28 +1000

    Initial commit

---
+++ web/src/pages/ConfirmReservationPage/ConfirmReservationPage.tsx
@@ -0,0 +1,11 @@
+import TwoColumnLayoutLayout from 'src/layouts/TwoColumnLayoutLayout/TwoColumnLayoutLayout'
+
+const ConfirmReservationPage = () => {
+  return (
+    <>
+      <TwoColumnLayoutLayout metaTitle="List Hubs"></TwoColumnLayoutLayout>
+    </>
+  )
+}
+
+export default ConfirmReservationPage
