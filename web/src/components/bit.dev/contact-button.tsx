292da36837225a696b8553aa8715c1aa03dfda7e
Tim Jones <t27.jones@connect.qut.edu.au>
2021-12-18 11:27:52 +1000

    Complete mobile responsive styles

--- web/src/components/bit.dev/contact-button.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from '@buildcities/build-ui.components.all'
-const ContactOwnerButton = () => {
-  return <Button text="Contact owner" style="secondary" />
-}
-
-export default ContactOwnerButton
--- web/src/pages/ConfirmReservationPage/ConfirmReservationPage.tsx
+++ web/src/pages/ConfirmReservationPage/ConfirmReservationPage.tsx
@@ -2,7 +2,7 @@ import SingleColumnLayout from 'src/layouts/SingleColumnLayout/SingleColumnLayou
 import MapPin from '../../utils/svgs/map-pin'
 import TempImage from './temp.jpg'
 import QRCode from './qr.png'
-import ContactOwnerButton from '../../components/bit.dev/contact-button'
+import { Button } from '@buildcities/build-ui.components.all'
 
 // Move to presets.ts later
 const HUB_NAME = 'Awesome place to stay'
@@ -13,7 +13,7 @@ const ConfirmReservationPage = () => {
   return (
     <>
       <SingleColumnLayout metaTitle="Reservation Confirmation">
-        <h1 className="text-white text-5xl font-medium mb-4 font-sans">
+        <h1 className="text-white md:text-5xl text-3xl font-medium mb-4 font-sans">
           {HUB_NAME}
         </h1>
         <div className="flex mb-7">
@@ -22,31 +22,35 @@ const ConfirmReservationPage = () => {
             Los Angeles, Hidden Hills, Ultra Secret Location
           </p>
         </div>
-        <div className="flex">
-          <div className="w-max rounded-lg overflow-hidden">
+        <div className="flex xl:flex-row flex-col">
+          <div className="xl:w-max xl:mx-0 sm:w-full mx-auto rounded-lg overflow-hidden">
             {/* Image goes here */}
-            <img src={TempImage} className="w-full h-full" alt={HUB_NAME} />
+            <img
+              src={TempImage}
+              className="xl:h-full xl:w-auto w-full rounded-lg"
+              alt={HUB_NAME}
+            />
           </div>
-          <div className="sm:w-[372px] ml-10 text-white">
+          <div className="xl:w-[372px] w-full xl:ml-10 xl:mt-0 xl:mb-0 mt-4 mb-9 mx-auto text-white">
             {/* Section */}
             <div className="mb-6">
-              <span className="block text-3xl mb-4 font-sans font-medium">
+              <span className="block md:text-3xl text-2xl mb-4 font-sans font-medium">
                 1. Dates
               </span>
-              <div className="flex rounded-lg bg-cardBackground border border-gray-400 w-full h-28">
-                <div className="p-4 border-r border-gray-400 w-1/2">
-                  <span className="block text-2xl font-fira mb-2.5">
+              <div className="flex rounded-lg bg-cardBackground border border-gray-400 w-full md:h-28 h-14">
+                <div className="md:p-4 pt-1 pl-4 border-r border-gray-400 w-1/2">
+                  <span className="block md:text-2xl text-sm font-sans md:mb-2.5 md:font-normal font-bold">
                     {CHECK_IN_TEXT}
                   </span>
-                  <span className="font-sans text-3xl font-medium">
+                  <span className="font-sans md:text-3xl text-sm md:font-bold">
                     10/10/22
                   </span>
                 </div>
-                <div className="p-4 w-1/2">
-                  <span className="block text-2xl font-fira mb-2.5">
+                <div className="md:p-4 pt-1 pl-4 w-1/2">
+                  <span className="block md:text-2xl text-sm font-sans md:mb-2.5 md:font-normal font-bold">
                     {CHECK_OUT_TEXT}
                   </span>
-                  <span className="font-sans text-3xl font-medium">
+                  <span className="font-sans md:text-3xl text-sm md:font-bold">
                     10/10/22
                   </span>
                 </div>
@@ -54,10 +58,10 @@ const ConfirmReservationPage = () => {
             </div>
             {/* Section */}
             <div className="mb-6">
-              <span className="block text-3xl mb-4 font-sans font-medium">
+              <span className="block md:text-3xl text-2xl mb-4 font-sans font-medium">
                 2. Your QR code
               </span>
-              <div className="rounded-lg bg-cardBackground p-3 flex items-center">
+              <div className="rounded-lg bg-cardBackground p-3 flex items-center justify-center">
                 <picture className="block w-[153px] h-[153px]">
                   <img src={QRCode} alt="QR code" className="w-full h-full" />
                 </picture>
@@ -65,7 +69,7 @@ const ConfirmReservationPage = () => {
                   <p className="mb-2 text-paragraph font-medium text-sm">
                     This code should be used as identifier, so keep it safe.
                   </p>
-                  <button className="flex items-center justify-center rounded-lg border border-blue h-12 w-44 font-medium text-sm">
+                  <button className="flex items-center justify-center rounded-lg border border-blue h-12 w-44 font-medium text-sm transition-colors duration-300 hover:bg-blue">
                     Download code
                   </button>
                 </div>
@@ -73,7 +77,7 @@ const ConfirmReservationPage = () => {
             </div>
             {/* Section */}
             <div className="mb-6">
-              <span className="block text-3xl font-sans font-medium text-paragraph">
+              <span className="block md:text-3xl text-2xl font-sans font-medium text-paragraph">
                 Leave note
               </span>
               <small className="text-sm text-paragraph font-medium">
@@ -82,12 +86,15 @@ const ConfirmReservationPage = () => {
               </small>
               <textarea
                 placeholder="Your note here"
-                className="rounded-lg border-dotted border-gray-800 bg-cardBackground w-full h-24 p-4 mt-4 flex items-center text-paragraph"
+                className="rounded-lg border-dashed border-2 border-gray-800 bg-cardBackground w-full h-24 p-4 mt-4 flex items-center text-paragraph"
               ></textarea>
             </div>
-
             <div className="flex">
-              <ContactOwnerButton />
+              <Button text="Contact owner" style="secondary" />
+              {/* Add in spacer as I can't edit the button styles
+                  - adding margin breaks the borders */}
+              <span className="w-10"></span>
+              <Button text="Continue" style="primary" />
             </div>
           </div>
         </div>
