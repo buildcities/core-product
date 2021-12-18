08c465e2f3e080e8149794cfd0006f448ac77491
Tim Jones <t27.jones@connect.qut.edu.au>
2021-12-18 12:03:08 +1000

    Add pencil icon

--- web/src/components/bit.dev/reservation-page/date-section.tsx
+++ web/src/components/bit.dev/reservation-page/date-section.tsx
@@ -1,10 +1,17 @@
 import { CHECK_IN_TEXT, CHECK_OUT_TEXT } from './presets'
+import { EditPencil } from 'src/utils/svgs'
+
 export default function DateSection() {
   return (
     <div className="mb-6">
-      <span className="block md:text-3xl text-2xl mb-4 font-sans font-medium">
-        1. Dates
-      </span>
+      <div className="flex md:items-center items-end mb-4">
+        <span className="block md:text-3xl text-2xl font-sans font-medium">
+          1. Dates
+        </span>
+        <button className="cursor-pointer">
+          <EditPencil className="md:w-6 md:h-6 md:ml-6 md:mb-0 ml-2.5 mb-1 w-4 h-4" />
+        </button>
+      </div>
       <div className="flex rounded-lg bg-cardBackground border border-gray-400 w-full md:h-28 h-14">
         <div className="md:p-4 pt-1 pl-4 border-r border-gray-400 w-1/2">
           <span className="block md:text-2xl text-sm font-sans md:mb-2.5 md:font-normal font-bold">
