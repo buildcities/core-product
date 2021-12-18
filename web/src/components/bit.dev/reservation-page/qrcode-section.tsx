c5d8a87da5d8c9901744ae021cb07c9105b2ec28
Tim Jones <t27.jones@connect.qut.edu.au>
2021-12-18 11:54:15 +1000

    Reverse direction of QR code div contents on mobile

--- web/src/components/bit.dev/reservation-page/qrcode-section.tsx
+++ web/src/components/bit.dev/reservation-page/qrcode-section.tsx
@@ -7,12 +7,12 @@ export default function QRCodeSection({ codeImage }: QRCodeSectionProps) {
       <span className="block md:text-3xl text-2xl mb-4 font-sans font-medium">
         2. Your QR code
       </span>
-      <div className="rounded-lg bg-cardBackground p-3 flex items-center justify-center">
+      <div className="rounded-lg bg-cardBackground p-3 flex items-center justify-center md:flex-row flex-row-reverse">
         <picture className="block w-[153px] h-[153px]">
           <img src={codeImage} alt="QR code" className="w-full h-full" />
         </picture>
-        <div className="ml-4 w-min">
-          <p className="mb-2 text-paragraph font-medium text-sm">
+        <div className="md:ml-4 mr-4 w-min">
+          <p className="md:mb-2 mb-4 text-paragraph font-medium text-sm">
             This code should be used as identifier, so keep it safe.
           </p>
           <button className="flex items-center justify-center rounded-lg border border-blue h-12 w-44 font-medium text-sm transition-colors duration-300 hover:bg-blue">
