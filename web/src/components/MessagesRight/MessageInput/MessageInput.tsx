307b3c142be0913b9d58098c41c5b3564a85be93
tumus2004 <41445493+tumus2004@users.noreply.github.com>
2021-12-27 13:36:02 +1000

    styling updates for messaging input

--- web/src/components/MessagesRight/MessageInput/MessageInput.tsx
+++ web/src/components/MessagesRight/MessageInput/MessageInput.tsx
@@ -9,8 +9,8 @@ type MessageInputProps = {
 
 const MessageInput = ({ avatar, address }: MessageInputProps) => {
   return (
-    <div className="relative">
-      <div className="block fixed bottom-[30px] w-1/2 max-w-[816px] mr-96 items-bottom bg-[#171717] rounded-xl px-4 py-2">
+    <div className="flex">
+      <div className="block fixed bottom-[30px] w-1/2 max-w-[814px] items-bottom bg-[#171717] rounded-xl px-4 py-2">
         <div className="flex items-end justify-left">
           <div className="items-center grid grid-rows-2 grid-flow-col text-left border-solid border-1 border-white">
             <img
