b321705a1a28fbe31d9b21c12b42774e2ba7f2de
Ronald Maduka <rolly.maduk@gmail.com>
2021-12-28 15:51:17 +0100

    FIX:discord auth login

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Heading, Button } from '@buildcities/build-ui.components.all'
 import SingleColumnLayout from 'src/layouts/SingleColumnLayout/SingleColumnLayout'
