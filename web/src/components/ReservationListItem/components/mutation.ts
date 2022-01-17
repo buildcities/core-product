e46cc2b7ae8ff9d27fba0611d91c5d4f5fffbcf5
Ronald Maduka <rolly.maduk@gmail.com>
2022-01-17 21:55:00 +0100

    FIX:fixes to types in reservation

--- web/src/components/ReservationListItem/components/mutation.ts
+++ web/src/components/ReservationListItem/components/mutation.ts
@@ -1,8 +1,5 @@
 export const UPDATE_RESERVATION = gql`
-  mutation UpdateReservationMutation(
-    $id: String!
-    $input: UpdateReservationInput!
-  ) {
+  mutation UpdateReservation($id: String!, $input: UpdateReservationInput!) {
     updateReservation(id: $id, input: $input) {
       id
     }
